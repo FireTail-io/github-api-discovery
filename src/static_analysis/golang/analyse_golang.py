333d7832ce15d4a464c50bc1af36a244a765f61b
theteacat <joshua@firetail.io>
2023-06-07 17:17:44 +0100

    Use ctypes to call go analyser

--- src/static_analysis/golang/analyse_golang.py
+++ src/static_analysis/golang/analyse_golang.py
@@ -1,2 +1,19 @@
+import ctypes
+import json
+from typing import Callable
+
+
+GOLANG_ANALYSIS_LIBRARY = ctypes.cdll.LoadLibrary("/golang_analyser/main.so")
+GOLANG_ANALYSER: Callable[[bytes, bytes], bytes] = GOLANG_ANALYSIS_LIBRARY.AnalyseCGOWrapper
+GOLANG_ANALYSER.argtypes = [ctypes.c_char_p, ctypes.c_char_p]  # type: ignore
+GOLANG_ANALYSER.restype = ctypes.c_void_p  # type: ignore
+
+
 def analyse_golang(file_path: str, file_contents: str) -> tuple[set[str], dict[str, dict]]:
-    return set(), {}
+    response_json_ptr = GOLANG_ANALYSER(file_path.encode("utf-8"), file_contents.encode("utf-8"))
+    loaded_response = json.loads(ctypes.string_at(response_json_ptr))
+
+    frameworks_identified = loaded_response["frameworks_identified"]
+    openapi_specs = loaded_response["openapi_specs"]
+
+    return set(frameworks_identified), openapi_specs
