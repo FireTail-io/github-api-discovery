df8974249f0bdb44342459a6b383b099b3e6a9cd
theteacat <joshua@firetail.io>
2023-06-09 11:44:28 +0100

    Add err handling to analyse_golang

--- src/static_analysis/golang/analyse_golang.py
+++ src/static_analysis/golang/analyse_golang.py
@@ -2,6 +2,7 @@ import ctypes
 import json
 from typing import Callable
 
+from utils import logger
 
 GOLANG_ANALYSIS_LIBRARY = ctypes.cdll.LoadLibrary("/analysers/golang/main.so")
 GOLANG_ANALYSER: Callable[[bytes, bytes], bytes] = GOLANG_ANALYSIS_LIBRARY.AnalyseCGOWrapper
@@ -15,6 +16,10 @@ def analyse_golang(file_path: str, file_contents: str) -> tuple[set[str], dict[s
 
     response_json_ptr = GOLANG_ANALYSER(file_path.encode("utf-8"), file_contents.encode("utf-8"))
     loaded_response = json.loads(ctypes.string_at(response_json_ptr))
+
+    if "error" in loaded_response:
+        logger.critical(f"Golang static analysis failed on '{file_path}', error: {loaded_response['error']}")
+        return set(), {}
 
     frameworks_identified = loaded_response["frameworks_identified"]
     openapi_specs = loaded_response["openapi_specs"]
