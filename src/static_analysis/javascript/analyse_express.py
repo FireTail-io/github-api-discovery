06f1bbc0da974afb6751b52dd5a94d69c416fe9a
theteacat <joshua@firetail.io>
2023-06-14 16:10:49 +0100

    Initial implementation of extracting paths and methods from express

--- src/static_analysis/javascript/analyse_express.py
+++ src/static_analysis/javascript/analyse_express.py
@@ -1,7 +1,7 @@
 from tree_sitter import Tree
 
 from static_analysis.javascript.utils import (
-    get_default_identifiers_from_import_statement, get_identifier_from_variable_declarator,
+    get_children_of_type, get_default_identifiers_from_import_statement, get_identifier_from_variable_declarator,
     get_module_name_from_import_statement, get_module_name_from_require_args,
     is_variable_declarator_or_assignment_expression_calling_func, traverse_tree_depth_first
 )
@@ -61,11 +61,74 @@ def get_router_identifiers(tree: Tree, express_identifiers: set[str]) -> set[str
     return set()
 
 
-def get_paths_and_methods(tree: Tree, app_and_router_identifiers: set[str]) -> dict[str, list[str]]:
+def get_paths_and_methods(tree: Tree, app_and_router_identifiers: set[str]) -> dict[str, set[str]]:
     # TODO: find calls to .all(), .route(), .checkout(), .copy(), .delete(), .get(), .head(), .lock(), .merge(),
     # .mkactivity(), .mkcol(), .move(), m-.search(), .notify(), .options(), .patch(), .post(), .purge(), .put(),
     # .report(), .search(), .subscribe(), .trace(), .unlock(), and .unsubscribe() on app and router identifiers
-    return {}
+    paths: dict[str, set[str]] = {}
+
+    SUPPORTED_EXPRESS_PROPERTIES = {
+        "all", "checkout", "copy", "delete", "get", "head", "lock", "merge", "mkactivity", "mkcol", "move", "notify",
+        "options", "patch", "post", "purge", "put", "report", "search", "subscribe", "trace", "unlock", "unsubscribe",
+        "use"
+    }
+
+    for node in traverse_tree_depth_first(tree):
+        match node.type:
+            case "call_expression":
+                # e.g. 'app.listen(port, () => {\n  console.log(`Example app listening on port ${port}`)\n})'
+                # where the member expression would be 'app.listen'
+                member_expressions = get_children_of_type(node, "member_expression")
+                if len(member_expressions) != 1:
+                    continue
+
+                # if the member expression is 'app.listen', it should have a direct identifier child for 'app'
+                member_expression_identifiers = get_children_of_type(member_expressions[0], "identifier")
+                if (
+                    len(member_expression_identifiers) != 1
+                    or type(member_expression_identifiers[0].text) != bytes
+                    or member_expression_identifiers[0].text.decode("utf-8") not in app_and_router_identifiers
+                ):
+                    continue
+
+                # if the member expression is 'app.listen', it should have a property identifier for 'listen'
+                property_identifiers = get_children_of_type(member_expressions[0], "property_identifier")
+                if (
+                    len(property_identifiers) != 1
+                    or type(property_identifiers[0].text) != bytes
+                    or property_identifiers[0].text.decode("utf-8") not in SUPPORTED_EXPRESS_PROPERTIES
+                ):
+                    continue
+
+                property = property_identifiers[0].text.decode("utf-8")
+                if property in ["all", "use"]:
+                    methods = SUPPORTED_EXPRESS_PROPERTIES.difference({"all", "use"})
+                else:
+                    methods = {property}
+
+                # The call_expression should have one arguments child node
+                arguments = get_children_of_type(node, "arguments")
+                if len(arguments) != 1:
+                    continue
+
+                # All the supported properties should have exactly one string argument, their path; except `.use` which
+                # may have no string arguments, in which case its path defaults to `/`
+                string_arguments = get_children_of_type(arguments[0], "string")
+                if len(string_arguments) == 0 and property == "use":
+                    paths["/"] = paths.get("/", set()).union(methods)
+                if len(string_arguments) == 1:
+                    # There should be a single string fragment within the string whose text is the path
+                    string_fragments = get_children_of_type(string_arguments[0], "string_fragment")
+                    if (
+                        len(string_fragments) != 1
+                        or type(string_fragments[0].text) != bytes
+                    ):
+                        continue
+
+                    path = string_fragments[0].text.decode("utf-8")
+                    paths[path] = paths.get(path, set()).union(methods)
+
+    return paths
 
 
 def analyse_express(tree: Tree) -> dict | None:
